4c9fcc3c90b4027f3c900486f3b5cbac7d5542e1
jason_st <majiao@pwrd.com>
2019-08-12 17:49:14 +0800

    增加Boid效果
    增加文件: SimpleBoid.compute, SimpleBoid.shader, SimpleBoid.cs, 相关材质球, 场景文件

---
+++ Assets/Script/SimpleBoid.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+
+public class SimpleBoid : MonoBehaviour
+{
+    public int BoidCount;
+    public Mesh mesh;
+    public Material material;
+    public ComputeShader cmpShader;
+
+    [Range(0, 50f)]
+    public float Speed;
+
+    [Range(0, 50f)]
+    public float BoidDistance;
+
+    public Transform Target;
+
+    private ComputeBuffer m_dataBuffer;
+    private ComputeBuffer m_argBuffer;
+    private int GroupX;
+    private int m_kernel;
+
+    private Bounds m_rangeBound;
+    private readonly Vector3 BOUND_CENTER;
+    private const int BOUND_SIZE = 10;
+
+    private struct BoidData
+    {
+        public Vector3 pos;
+        public Vector3 rot;
+        public Vector3 scale;
+    }
+
+    void Start()
+    {
+        if (BoidCount == 0 || material == null || cmpShader == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        m_kernel = cmpShader.FindKernel("CSMain");
+        if (m_kernel == -1)
+        {
+            enabled = false;
+            return;
+        }
+
+        int threadGroupSizeX = 256;
+        // key, 注意乘以 1.0f //
+        GroupX = Mathf.CeilToInt(BoidCount*1.0f / threadGroupSizeX);
+
+        // init array //
+        BoidData[] dataArray = InitBoidData();
+
+        // data buffer //
+        const int STRIDE = (3 + 3 + 3) * sizeof(float);
+        m_dataBuffer = new ComputeBuffer(BoidCount, STRIDE);
+        m_dataBuffer.SetData(dataArray);
+
+        // set parameter //
+        cmpShader.SetBuffer(m_kernel, "_BoidDataBuffer", m_dataBuffer);
+        cmpShader.SetInt("_BoidCount", BoidCount);
+        
+        material.SetBuffer("_BoidDataBuffer", m_dataBuffer);
+
+        // arg buffer //
+        int[] args = new int[5]
+        {
+            (int)mesh.GetIndexCount(0),
+            BoidCount,
+            (int)mesh.GetIndexStart(0),
+            (int)mesh.GetBaseVertex(0),
+            0
+        };
+
+        m_argBuffer = new ComputeBuffer(1, 5 * sizeof(int), ComputeBufferType.IndirectArguments);
+        m_argBuffer.SetData(args);
+
+        // 全部物体所在的范围 //
+        m_rangeBound = new Bounds(BOUND_CENTER, new Vector3(BOUND_SIZE * 2, BOUND_SIZE * 2, BOUND_SIZE * 2));
+    }
+
+    void Update()
+    {
+        cmpShader.SetFloat("_Speed", Speed);
+        cmpShader.SetFloat("_DeltaTime", Time.deltaTime);
+        cmpShader.Dispatch(m_kernel, GroupX, 1, 1);
+        cmpShader.SetFloat("_BoidDistance", BoidDistance);
+
+        if (Target != null)
+        {
+            cmpShader.SetVector("_TargetPos", Target.position);
+        }
+
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, m_rangeBound, m_argBuffer);
+    }
+
+    private BoidData[] InitBoidData()
+    {
+        BoidData[] dataArray = new BoidData[BoidCount];
+        for (int i = 0; i < BoidCount; i++)
+        {
+            Vector3 translation = new Vector3(Random.value, Random.value, Random.value) * 2 - Vector3.one;
+            translation *= BOUND_SIZE;
+            dataArray[i].pos = translation;
+
+            dataArray[i].rot = new Vector3(0,0,1); // new Vector3(Random.value, Random.value, Random.value) * 360;
+            dataArray[i].scale = Vector3.one;
+        }
+
+        return dataArray;
+    }
+
+    void OnDisable()
+    {
+        if (m_dataBuffer != null)
+        {
+            m_dataBuffer.Release();
+            m_dataBuffer = null;
+        }
+
+        if (m_argBuffer != null)
+        {
+            m_argBuffer.Release();
+            m_argBuffer = null;
+        }
+    }
+}
---
+++ Assets/Shader/SimpleBoid.shader
@@ -0,0 +1,81 @@
+﻿Shader "MJ/SimpleBoid"
+{
+    Properties
+    {
+        _MainColor("Main Color(RGB)", Color) = (1,1,1,1)
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #pragma target 4.5
+
+            #include "UnityCG.cginc"
+            #include "UnityInstancing.cginc"
+            
+            struct BoidData
+            {
+                float3 pos;
+                float3 rot;
+                float3 scale;
+            };
+
+        #if SHADER_TARGET >= 45
+            StructuredBuffer<BoidData> _BoidDataBuffer;
+        #endif
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float4 _MainColor;
+
+            v2f vert (appdata v, uint instanceID : SV_InstanceID)
+            {
+                v2f o;
+                
+                #if SHADER_TARGET >= 45
+                {
+                    BoidData data = _BoidDataBuffer[instanceID];
+                    float3 worldPos = v.vertex.xyz * data.scale + data.pos;
+
+                    o.vertex = mul(UNITY_MATRIX_VP, float4(worldPos, 1.0f));
+                }
+                #else
+                {
+                    o.vertex = UnityObjectToClipPos(v.vertex);
+                }
+                #endif
+                
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                float4 col = tex2D(_MainTex, i.uv) * _MainColor;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
